67207cb8af1b065a73a23485a9f00a8492f4c851
Sandeep Jagtap <sandeep.jagtap@gmail.com>
2012-05-13 18:28:17 -0500

    using emacs 24 package manager. starter-kit-ruby, rinari, js2, js-comint,auto-complete. rsense is broken. run-js working with js-comint

---
+++ emacs24dotfiles/.emacs.d/init.el
@@ -0,0 +1,69 @@
+(require 'package)
+
+;;following line for elpa package needs to be uncommented to get rinari package working
+;;(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar custom-packages '(
+                      starter-kit
+                      starter-kit-ruby
+                      flymake-ruby
+                      ruby-block
+                      js2-mode
+                      js-comint
+                      rinari
+                      auto-complete
+                      haml-mode
+                      apache-mode
+                      crontab-mode
+                      puppet-mode
+                      memory-usage)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p custom-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+
+
+(setq js-rhino-path (concat "java -jar " (expand-file-name "~/.emacs.d/libs/rhinojs.jar")))
+
+(setq inferior-js-program-command  js-rhino-path)
+(add-hook 'js2-mode-hook '(lambda () 
+			    (local-set-key "\C-x\C-e" 'js-send-last-sexp)
+			    (local-set-key "\C-\M-x" 'js-send-last-sexp-and-go)
+			    (local-set-key "\C-cb" 'js-send-buffer)
+			    (local-set-key "\C-c\C-b" 'js-send-buffer-and-go)
+			    (local-set-key "\C-cl" 'js-load-file-and-go)
+			    ))
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
+(ac-config-default)
+
+;;rsense not working with emacs24 test beta
+;;(setq rsense-home  (expand-file-name "~/.emacs.d/libs/rsense-0.3"))
+;;(add-to-list 'load-path (concat rsense-home "/etc"))
+;;(require 'rsense)
+
+;; Complete by C-c .
+;;(add-hook 'ruby-mode-hook
+;;          (lambda ()
+;;            (local-set-key (kbd "C-c .") 'ac-complete-rsense)))
+
+
+
+(setq vendor-dir (concat (expand-file-name "~/.emacs.d") "/vendor"))
+(add-to-list 'load-path vendor-dir)
+
+(require 'grep-a-lot)
+(grep-a-lot-setup-keys)
+
+(set-frame-position (selected-frame) 0 0)
+;;(set-frame-size (selected-frame) 50000 50000)
